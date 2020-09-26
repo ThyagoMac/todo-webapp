089cf8968e55d75f13225a9361497229eb9b0271
ThyagoMac <thyago.cormick@gmail.com>
2020-09-26 16:10:03 -0300

    Add + remove todo working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
   <div id="app">
-    Pipi
+    <div class="container grid-xs py-2">
+      <h3 class="title">Todo Web App</h3>
+      <form @submit.prevent="addTodo(todo)">
+        <div class="input-group">
+          <input type="text" class="form-input" placeholder="New Todo" v-model="todo.description">
+          <button class="btn btn-primary input-group-btn">Add</button>
+        </div>
+      </form>
+    </div>
+      <hr>
+      <div class="todo-list">
+        <Todo v-for="todo in todos" :key="todo.id" :todoComp="todo" @toggle="toggleTodo(todo)" @remove="removeTodo"/>
+      </div>
     
   </div>
 </template>
 
 <script>
 
+import  Todo from './components/todo'
 
 export default {
   name: 'App',
-  components: {
-    
-  }
+  components: { Todo },
+  data() {
+    return {
+      todos: [],
+      todo: { checked: false}
+    }
+  },
+  methods: {
+    toggleTodo(todo){
+      const index = this.todos.findIndex(todos => todos.id === todo.id);
+      if (index > -1){
+        const checked = !this.todos[index].checked;
+        this.$set(this.todos, index, {...this.todos[index], checked});
+        //this.todos[index].checked = checked;
+      }
+      
+    },
+    addTodo(todo){
+      todo.id = Date.now();
+      this.todos.push(todo);
+      this.todo = {checked: false};
+    },
+    removeTodo(todo){
+      const index = this.todos.findIndex(todos => todos.id === todo.id);
+      if(index > -1){
+        this.$delete(this.todos, index);
+        //this.todos.splice(index,1);
+
+      }
+    },
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.title{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+.todo-list{
+  padding-top: 2em;
+}
+
 </style>
---
+++ src/components/todo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="title flex-centered" :class="{checked: todoComp.checked}">
+          
+          <div class="title-icon">
+            <i class="icon" :class="todoComp.checked ? 'icon-check' : 'icon-time'"></i>
+          </div>
+          <div class="title-concent mx-2">
+            <div class="title-subtitle">{{ todoComp.description }}</div>
+          </div>
+          <div class="title-action">
+            <button class="btn btn-link mx-1" @click="$emit('toggle', todoComp)">
+                <!-- 
+                <span class="text-success" v-show="!todoComp.checked"><i class="icon icon-check"></i></span>
+                <span class="text-error" v-show="todoComp.checked"><i class="icon icon-cross"></i></span> 
+                -->
+                <span class="text-success" v-if="!todoComp.checked"><i class="icon icon-check"></i></span>
+                <span class="text-error" v-else><i class="icon icon-cross"></i></span>
+            </button>
+            <button class="btn btn-link mx-1"><i class="icon icon-edit"></i></button>
+            <button class="btn btn-link mx-1" @click="$emit('remove', todoComp)"><span class="text-error"><i class="icon icon-delete"></i></span></button>
+          </div>
+          
+        </div>
+</template>
+
+<script>
+export default {
+    props: {
+        todoComp: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.checked{
+    text-decoration: line-through;
+    color: lightgray;
+}
+</style>
